6e0351e825af1f9bc50e87b2c7665682a700c767
Мартышов Данила Викторович <409091@niuitmo.ru>
2024-03-05 01:36:52 +0300

    removed deprecated implementations

--- src/main/java/org/example/classes/Creature.java
+++ src/main/java/org/example/classes/Creature.java
@@ -2,16 +2,8 @@ package org.example.classes;
 
 import org.example.interfaces.*;
 
-public class Creature extends Animal implements Resembleable, Speculatable {
+public class Creature extends Animal{
     public Creature(String name){
         super(name,false);
-    }
-    @Override
-    public void resemble(String what){
-        System.out.println(howIsNamed + " напоминает " + what);
-    }
-    @Override
-    public void speculate(String aboutWhat){
-        System.out.println("Считается, что " + howIsNamed + " " + aboutWhat);
     }
 }
--- src/main/java/org/example/classes/Stalagmite.java
+++ src/main/java/org/example/classes/Stalagmite.java
@@ -2,15 +2,8 @@ package org.example.classes;
 
 import org.example.interfaces.*;
 
-public class Stalagmite extends  GeologicalFormation implements Breakable {
+public class Stalagmite extends  GeologicalFormation{
     public Stalagmite(String type, StoneDurability durability){
         super(type,durability);
-    }
-    @Override
-    public void breakSelf(){
-        System.out.println(type + " Откололись ");
-    }
-    public void hang(String where){
-        System.out.println(type + " нависают "+ where );
     }
 }
