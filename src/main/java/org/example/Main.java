95db4240a9b7d7f1fca03b27354b5c2bb2b84bc2
Мартышов Данила Викторович <409091@niuitmo.ru>
2024-03-19 00:59:32 +0300

    started work on client-server architecture

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,17 @@ import org.example.interfaces.ReadMarkedField;
 
 import java.io.*;
 import java.lang.reflect.*;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.lang.annotation.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Main {
+    public Main() throws IOException {
+    }
+
     static class MarkedExporter{           // статический класс
         public MarkedExporter(){
         }
@@ -78,36 +85,71 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         //creating instances and setting stats
-        CommandLine cl = new CommandLine();
-        var cls = new CollectionLoaderSaver("ans.txt",cl);
-        var cm = new CollectionManager(cls);
-        cm.initialaze();
-        cl.printLn("\n");
-        cl.printLn("Welcome back. Enter 'help' to see information on available commands");
-        var com = new CommandManager();
-        var re = new RuntimeEnv(cl,com);
-        com.getCommandList().put("add", new Add(cl,cm));
-        com.getCommandList().put("clear", new Clear(cl,cm));
-        com.getCommandList().put("info", new Info(cl,cm));
-        com.getCommandList().put("show", new Show(cl,cm));
-        com.getCommandList().put("exit", new Exit(cl,cm));
-        com.getCommandList().put("sort", new Sort(cl,cm));
-        com.getCommandList().put("update", new Update(cl,cm));
-        com.getCommandList().put("remove_by_id", new Remove(cl,cm));
-        com.getCommandList().put("filter_by_is_alive", new FilterByIsAlive(cl,cm));
-        com.getCommandList().put("save", new Save(cl,cm));
-        com.getCommandList().put("filter_by_less_than_number_of_dug_counter", new FilterLessDC(cl,cm));
-        com.getCommandList().put("count_by_researcher_type", new CountByResearcherType(cl,cm));
-        com.getCommandList().put("remove_lower", new RemoveLower(cl,cm));
-        com.getCommandList().put("insert", new Insert(cl,cm));
-        com.getCommandList().put("execute_script", new ExecuteScript(cl,cm));
-        com.getCommandList().put("get_history", new GetHistory(cl,com));
-        com.getCommandList().put("help", new Help(cl,com));
-        com.getCommandList().put("redo",new Redo(cl,com,re));
-        com.getCommandList().put("redo_f",new RedoFile(cl,com,re));
-        re.mannedMode();
+//        CommandLine cl = new CommandLine();
+//        var cls = new CollectionLoaderSaver("ans.txt",cl);
+//        var cm = new CollectionManager(cls);
+//        cm.initialaze();
+//        cl.printLn("\n");
+//        cl.printLn("Welcome back. Enter 'help' to see information on available commands");
+//        var com = new CommandManager();
+//        var re = new RuntimeEnv(cl,com);
+//        com.getCommandList().put("add", new Add(cl,cm));
+//        com.getCommandList().put("clear", new Clear(cl,cm));
+//        com.getCommandList().put("info", new Info(cl,cm));
+//        com.getCommandList().put("show", new Show(cl,cm));
+//        com.getCommandList().put("exit", new Exit(cl,cm));
+//        com.getCommandList().put("sort", new Sort(cl,cm));
+//        com.getCommandList().put("update", new Update(cl,cm));
+//        com.getCommandList().put("remove_by_id", new Remove(cl,cm));
+//        com.getCommandList().put("filter_by_is_alive", new FilterByIsAlive(cl,cm));
+//        com.getCommandList().put("save", new Save(cl,cm));
+//        com.getCommandList().put("filter_by_less_than_number_of_dug_counter", new FilterLessDC(cl,cm));
+//        com.getCommandList().put("count_by_researcher_type", new CountByResearcherType(cl,cm));
+//        com.getCommandList().put("remove_lower", new RemoveLower(cl,cm));
+//        com.getCommandList().put("insert", new Insert(cl,cm));
+//        com.getCommandList().put("execute_script", new ExecuteScript(cl,cm));
+//        com.getCommandList().put("get_history", new GetHistory(cl,com));
+//        com.getCommandList().put("help", new Help(cl,com));
+//        com.getCommandList().put("redo",new Redo(cl,com,re));
+//        com.getCommandList().put("redo_f",new RedoFile(cl,com,re));
+////        re.mannedMode();
+//        class Product{
+//            String name;
+//            Double price;
+//            public double getPrice() {return price;}
+//            public Product(String name, double price){
+//                this.name=name;
+//                this.price = price;
+//            }
+//
+//            public String getName() {
+//                return name;
+//            }
+//        }
+//        List<Product> ls = new ArrayList<>();
+//        ls.add(new Product("ass",300));
+//        ls.add(new Product("pussy",500));
+//        Optional<String> cn = ls.stream().map(Product::getName).reduce((a, b) -> a+b);
+//        System.out.println(cn.toString());
+
+    Socket socket = new Socket("localhost", 6969);
+    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+    BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+    String userInput;
+
+        while ((userInput = stdIn.readLine()) != null) {
+        out.println(userInput);
+        System.out.println("Server: " + in.readLine());
+    }
+
+        out.close();
+        in.close();
+        stdIn.close();
+        socket.close();
     }
 }
